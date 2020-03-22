45ed87d7fb834a86a9500b90d8f2fb39a81af66d
Alexis Garcia <45220725+AlexisGs09@users.noreply.github.com>
2020-03-22 17:50:09 -0600

    Create Servidor.cpp

---
+++ Sockets/Servidor.cpp
@@ -0,0 +1,29 @@
+#include "SocketDatagrama.h"
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <netdb.h>
+using namespace std;
+int main(){
+	int res,clilen;
+	struct sockaddr_in server_addr, msg_to_client_addr;
+	//Crear el objeto SocketDatagrama
+	/*se asigna una direccion al socket del servidor*/
+	bzero((char *)&server_addr,sizeof(server_addr));
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_addr.s_addr=INADDR_ANY;
+	server_addr.sin_port=htons(puerto);
+	bind(s,(struct sockaddr *)&server_addr,sizeof(server_addr));
+	clilen=sizeof(msg_to_client_addr);
+	while(1){
+		//Llamar a la función recibe
+
+		/*envia la peticion al cliente. La estructura msg_to_client_addr contiene
+		la direccion socket del cliente*/
+		//Llamar a la funcion enviar 
+		send_to(s,(char *)&res, sizeof(int), 0,(struct sockaddr*)&msg_to_client_addr,clilen);
+	}
+	return 0;
+}
