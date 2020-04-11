74895eebf6af37175bc55c2b903525d9b00b3c10
Alexis Garcia <45220725+AlexisGs09@users.noreply.github.com>
2020-04-10 21:37:17 -0500

    Create Respuesta.h

---
+++ Solicitud-Respuesta Confiable/Respuesta.h
@@ -0,0 +1,15 @@
+#include "Mensaje.h"
+#include "SocketDatagrama.h"
+class Respuesta
+{
+public:
+    Respuesta(int port);
+    struct mensaje *getRequest(void);
+    void sendReply(char *respuesta);
+
+private:
+    SocketDatagrama *socketlocal;
+    PaqueteDatagrama *clientePaqueteDatagrama;
+    struct mensaje petition;
+    int contador;
+};
